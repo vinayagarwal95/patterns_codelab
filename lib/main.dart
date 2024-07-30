3e2c2f7462936e37300a31732b6dcd3fe3bde2d5
Vinay Personal <52975722+vinayagarwal95@users.noreply.github.com>
2024-07-30 23:30:12 +0530

    added files and made changes to implement the records concept

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'data.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const DocumentApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: DocumentScreen(
+        document: Document(),
+      ),
+    );
+  }
+}
+
+class DocumentScreen extends StatelessWidget {
+  final Document document;
+
+  const DocumentScreen({
+    required this.document,
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    final metaDataRecord = document.metadata;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(metaDataRecord.$1),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Last modified ${metaDataRecord.modified}',
+            ),
+          ),
+        ],
       ),
     );
   }
